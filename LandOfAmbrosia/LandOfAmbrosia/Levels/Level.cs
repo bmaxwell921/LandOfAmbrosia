98cc2a4301241258618a316fb50fa7b356bc0a80
bmaxwell921 <bmaxwell921@gmail.com>
2013-03-25 23:46:25 -0500

    Added code for levels

--- LandOfAmbrosia/LandOfAmbrosia/Levels/Level.cs
+++ LandOfAmbrosia/LandOfAmbrosia/Levels/Level.cs
@@ -7,5 +7,49 @@ namespace LandOfAmbrosia.Levels
 {
     class Level
     {
+        private const int DEFAULT_WIDTH = 100;
+        private const int DEFAULT_HEIGHT = 20;
+        
+        int width, height;
+
+        public Tile[,] tiles;
+        //private Skybox skybox;
+
+        /// <summary>
+        /// Creates a new level with the default height and width
+        /// </summary>
+        public Level() : this(DEFAULT_WIDTH, DEFAULT_HEIGHT)
+        {
+        }
+
+        /// <summary>
+        /// Creates an empty level with the given width and height
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public Level(int width, int height)
+        {
+            tiles = new Tile[width, height];
+            this.SetUpSkybox();
+        }
+
+        private void SetUpSkybox()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetTile(int width, int height, Tile newTile)
+        {
+            if (width < 0 || width > this.width || height < 0 || height > this.height)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            tiles[width, height] = newTile; 
+        }
+
+        public void Draw(Camera c)
+        {
+            
+        }
     }
 }
--- LandOfAmbrosia/LandOfAmbrosia/Levels/Tile.cs
+++ LandOfAmbrosia/LandOfAmbrosia/Levels/Tile.cs
@@ -2,10 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using LandOfAmbrosia.Characters;
 
 namespace LandOfAmbrosia.Levels
 {
-    class Tile
+    class Tile : ICollidable
     {
+        public Vector3 location;
+        public Model model;
+        public Matrix world = Matrix.Identity;
+
+        public Tile(Vector3 location, Model model)
+        {
+            this.location = location;
+            this.model = model;
+        }
+
+        public void Draw(Camera c)
+        {
+            Matrix[] transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect be in mesh.Effects)
+                {
+                    be.EnableDefaultLighting();
+                    be.Projection = c.projection;
+                    be.View = c.view;
+                    be.World = world * mesh.ParentBone.Transform;
+                }
+
+                mesh.Draw();
+            }
+        }
+
+        public bool CollidesWith(ICollidable other)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
